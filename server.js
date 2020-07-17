9ac00c79a15a62642cabe1127929286f8b7406b9
michaeldavidpryor <mdpcaps@gmail.com>
2020-07-17 02:52:42 -0400

    cool

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 1234;
+const PORT = process.env || 3000;
 
 const app = express();
 
